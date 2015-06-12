a1489df61cb1265efdd2b0412c71d18590a6b4c7
cazanon <carlos.azanon@gmail.com>
2015-06-12 12:44:29 +0200

    Update recipe json structure

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,61 +1,177 @@
 var recipes = [    
-    {
+{
     "id":0,
     "name": "Paella Mixta",
-    "subname": "Arroz con verduras y pollo todo muy rico!",
+    "subname": "Paella mixta de verduras, pollo y marisco",
     "category": "Arroces",
+    "people": "4",
     "ingredients": [
         {
-       "ingredient": "Pollo",
-       "quantity": "200gr",
-       "details": "Cortado en tacos"
-        }, {
         "ingredient": "Arroz",
-        "quantity": "500gr",
-        "details": "Arroz bomba"
-        }
-    ],
-    "important": "true",
-    "favourite": [
-        {
-        "favList": "Diario",
+        "quantity": "500",
+        "units" : "gr",
+        "details": ""
         }, {
-        "favList": "Fines de semana",
-        }
-    ],
+        "ingredient": "Pollo",
+        "quantity": "300",
+        "units" : "gr",
+        "details": "Contramuslo deshuesado"
+		}, {
+	    "ingredient": "Gambas",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Peladas"
+		}, {
+	    "ingredient": "Mejillones",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Limpios"
+		}, {
+	    "ingredient": "Almejas/Chirlas",
+		"quantity": "200",
+        "units" : "gr",
+		"details": "Limpiarlas antes"
+		}, {
+	    "ingredient": "Sepia",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Cortada en taquitos"
+		}, {
+	    "ingredient": "Tomate frito",
+		"quantity": "1",
+        "units" : "lata de 200gr",
+		"details": ""
+		}, {
+	    "ingredient": "Cebolla",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Muy picada"
+		}, {
+	    "ingredient": "Pimiento verde",
+		"quantity": "2",
+        "units" : "unidades",
+		"details": "Muy picado"
+		}, {
+	    "ingredient": "Ajo",
+		"quantity": "1",
+        "units" : "diente",
+		"details": "Laminado"
+		}
+    ],        
     "step": [
         {
         "number": 1,
-        "sumary":"sumary del paso 1",
-        "description": "hay que freir el pollo",
-        "image": "img1"
+        "sumary":"Preparar los ingredientes",
+        "description": "En primer lugar se tienen que limpiar y después cortar todos los ingredientes. Los pimientos rojos se cortan en tiras largas, la cebolla en cubos y el ajo en rodajas finas. La carne se debe cortar en trozos  iguales para que se haga bien. La sepia se corta en trozos más pequeños.",
         }, {
         "number": 2,
-        "sumary":"sumary del paso 2",
-        "description": "se añade el arroz",
-        "image": "img2"
-        }
+        "sumary":"Freir la carne y la sepia",
+        "description": "A continuación se fríe la carne. Antes de que esté bien echa se añade la sepia y poco después la verdura. Se fríe todo hasta que la carne tenga un color dorado. Hay que controlar bien el fuego para que no se haga demasiado la carne y que la sepia quede blanda. Se pueden sacar algunos ingredientes de la paella y dejarlos en un plato hasta que el resto esté listo para seguir",
+        }, {
+        "number": 3,
+        "sumary":"Freir el resto de ingredientes",
+        "description": "A continuación se añade el resto de verdura y el ajo. De nuevo hay que controlar bien el fuego para no quemar ningún ingrediente de la paella mixta. Ahora también se añade la fritada (o sofrito) y el pimentón en polvo. Se deja freír todo durante unos minutos. Este es uno de los últimos pasos antes de añadir el agua o caldo y el arroz",
+        }, {
+        "number": 4,
+        "sumary":"Añadir caldo",
+        "description": "Para ésta receta se añadirían dos litros de agua. Si se usa arroz bomba se echaría un poco más de agua. De momento no hay nada más que hacer que esperar a que el agua comienze a hervir fuertemente para que la verdura cruda se pueda hacer bien. Sustituyendo el agua por caldo de pollo la paella mixta tendrá un sabor más intensivo",
+        }, {
+        "number": 5,
+        "sumary":"Añadir arroz y marisco",
+        "description": "Se puede echar arroz redondo normal o arroz bomba. Éste último absorbe más caldo y se dice que tiene un sabor mejor. Hay que añadir el arroz de forma que se reparta de forma uniforme en toda la paella y que quede cubierto completamente de agua. A veces puede ser necesario poner un poco de agua adicional. Acto seguido se añaden los mejillones y las gambas a la paella",
+        }, {
+        "number": 6,
+        "sumary":"Dar el color típico",
+        "description": "La paella siempre tiene que tener un color amarillento o dorado. Para ello se puede usar colorante alimentario o azafrán. Para que la paella tome un color uniforme hay que repartir bien el colorante. Ahora se puede probar el caldo para eventualmente añadir algún ingrediente más. Durante la fase de cocción no se puede remover ya el arroz. Esto es una regla muy importante a la hora de cocinar cualquier paella",
+        }, {
+        "number": 7,
+        "sumary":"El toque final",
+        "description": "La paella mixta estará lista para comer una vez ya no quede caldo en la aella y el arroz esté bien echo. Se recomienda dejar reposar la paella mixta durante unos miutos antes de comenzar a comer. A los valencianos les gusta dejar la paella al fuego un poco más de tiempo con el fin de tostar ligeramente la última capa de arroz. En éste caso hay que tener cuidado de no quemar el arroz"
+        }        
     ],
     "image": "img/fideua.jpg",
-    "icon": "img/arroz_icon.jpg"
-    },
-    
-    {
+    "icon": "img/arroz_icon.jpg",
+    "important": "true",
+    "favourite": [
+        {
+        "favList": "Diario",
+        }, {
+        "favList": "Fines de semana",
+        }
+    ]
+},
+{
     "id":1,
-    "name": "Espaguettis",
-    "subname": "Arroz con verduras y pollo todo muy rico!",
-    "category": "Arroces",
+    "name": "Espaguetis carbonara",
+    "subname": "Pasta con salsa carbonara",
+    "category": "Pastas",
+    "people": "4",
     "ingredients": [
         {
-       "ingredient": "Pollo",
-       "quantity": "200gr",
-       "details": "Cortado en tacos"
+        "ingredient": "Espaguetis",
+        "quantity": "300",
+        "units" : "gr",
+        "details": "Vale cualquier tipo de pasta"
         }, {
-        "ingredient": "Arroz",
-        "quantity": "500gr",
-        "details": "Arroz bomba"
-        }
+        "ingredient": "Cebolla",
+        "quantity": "1",
+        "units" : "unidad",
+        "details": "Muy picadita"
+		}, {
+	    "ingredient": "Beacon",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Locheado y cortado en trozos"
+		}, {
+	    "ingredient": "Queso curado",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Rayado"
+		}, {
+	    "ingredient": "Nata liquida/Leche descremada",
+		"quantity": "200",
+        "units" : "gr",
+		"details": ""
+		}, {
+	    "ingredient": "Huevo",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Solo la yema batida"
+		}
+    ],        
+    "step": [
+        {
+        "number": 1,
+        "sumary":"Preparar los ingredientes",
+        "description": "Cortar la cebolla, preparar el beacon y batir la yema hasta que esté bien liquida",
+        },{
+        "number": 2,
+        "sumary":"Cocer la pasta",
+        "description": "Se cuece la pasta en agua hirviendo con un poco de sal y aceite. Lo mejor es seguir el tiempo indicado en el envase",
+        },{
+        "number": 3,
+        "sumary":"Freir la cebolla y el beacon",
+        "description": "A continuación se fríe la La cebolla hasta que quede bien tierna. A mitad de cocción se añade el beacon para que quede crujiente sin que quede quemado. En este punto sazonar a gusto y añadir pimienta molida",
+        }, {
+        "number": 4,
+        "sumary":"Añadir la nata liquida",
+        "description": "Se añade la nata a nuestra cebolla y beacon. Cuando la salsa comienze a hervir, se añade el queso rayado sin dejar de remover para que se funda bien",
+        }, {
+        "number": 5,
+        "sumary":"Añadir la yema de huevo",
+        "description": "Se añae la yema de huevo a la salsa para que tenga mas consistencia y sabor. Se prueba y se añade mas sal y pimienta hasta que se encuentre a tu gusto",
+        }, {
+        "number": 6,
+        "sumary":"Mezclar con la pasta",
+        "description": "Una vez que tengamos nuestra salsa a gusto, se añade la pasta cocida y se mezcla todo bien para impregnar todos los espaguetis. Ir removiendo hasta que la pasta se caliente",
+        }, {
+        "number": 7,
+        "sumary":"Emplatar y servir",
+        "description": "Retirar del fuego y servir en raciones iguales. A disfrutar!",
+        }        
     ],
+    "image": "img/carbonara.jpg",
+    "icon": "img/pasta_icon.png",
     "important": "true",
     "favourite": [
         {
@@ -63,40 +179,100 @@ var recipes = [
         }, {
         "favList": "Fines de semana",
         }
-    ],
-    "step": [
-        {
-        "number": 1,
-        "sumary":"sumary del paso 1",
-        "description": "hay que freir el pollo",
-        "image": "img1"
-        }, {
-        "number": 2,
-        "sumary":"sumary del paso 2",
-        "description": "se añade el arroz",
-        "image": "img2"
-        }
-    ],
-        "image": "img/carbonara.jpg",
-        "icon": "img/pasta_icon.png"
-    },
-    
-    {
+    ]
+},
+{
     "id":2,
-    "name": "Bizcocho",
-    "subname": "Arroz con verduras y pollo todo muy rico!",
-    "category": "Arroces",
+    "name": "Solomillo al horno",
+    "subname": "Paella mixta de verduras, pollo y marisco.",
+    "category": "Carnes",
+    "people": "4",
     "ingredients": [
         {
-       "ingredient": "Pollo",
-       "quantity": "200gr",
-       "details": "Cortado en tacos"
-        }, {
         "ingredient": "Arroz",
-        "quantity": "500gr",
-        "details": "Arroz bomba"
-        }
+        "quantity": "500",
+        "units" : "gr",
+        "details": ""
+        }, {
+        "ingredient": "Pollo",
+        "quantity": "300",
+        "units" : "gr",
+        "details": "Contramuslo deshuesado"
+		}, {
+	    "ingredient": "Gambas",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Peladas"
+		}, {
+	    "ingredient": "Mejillones",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Limpios"
+		}, {
+	    "ingredient": "Almejas/Chirlas",
+		"quantity": "200",
+        "units" : "gr",
+		"details": "Limpiarlas antes"
+		}, {
+	    "ingredient": "Sepia",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Cortada en taquitos"
+		}, {
+	    "ingredient": "Tomate frito",
+		"quantity": "1",
+        "units" : "lata de 200gr",
+		"details": ""
+		}, {
+	    "ingredient": "Cebolla",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Muy picada"
+		}, {
+	    "ingredient": "Pimiento verde",
+		"quantity": "2",
+        "units" : "unidades",
+		"details": "Muy picado"
+		}, {
+	    "ingredient": "Ajo",
+		"quantity": "1",
+        "units" : "diente",
+		"details": "Laminado"
+		}
+    ],        
+    "step": [
+        {
+        "number": 1,
+        "sumary":"Preparar los ingredientes",
+        "description": "En primer lugar se tienen que limpiar y después cortar todos los ingredientes. Los pimientos rojos se cortan en tiras largas, la cebolla en cubos y el ajo en rodajas finas. La carne se debe cortar en trozos  iguales para que se haga bien. La sepia se corta en trozos más pequeños.",
+        }, {
+        "number": 2,
+        "sumary":"Freir la carne y la sepia",
+        "description": "A continuación se fríe la carne. Antes de que esté bien echa se añade la sepia y poco después la verdura. Se fríe todo hasta que la carne tenga un color dorado. Hay que controlar bien el fuego para que no se haga demasiado la carne y que la sepia quede blanda. Se pueden sacar algunos ingredientes de la paella y dejarlos en un plato hasta que el resto esté listo para seguir.",
+        }, {
+        "number": 3,
+        "sumary":"Freir el resto de ingredientes",
+        "description": "A continuación se añade el resto de verdura y el ajo. De nuevo hay que controlar bien el fuego para no quemar ningún ingrediente de la paella mixta. Ahora también se añade la fritada (o sofrito) y el pimentón en polvo. Se deja freír todo durante unos minutos. Este es uno de los últimos pasos antes de añadir el agua o caldo y el arroz.",
+        }, {
+        "number": 4,
+        "sumary":"Añadir caldo",
+        "description": "Para ésta receta se añadirían dos litros de agua. Si se usa arroz bomba se echaría un poco más de agua. De momento no hay nada más que hacer que esperar a que el agua comienze a hervir fuertemente para que la verdura cruda se pueda hacer bien. Sustituyendo el agua por caldo de pollo la paella mixta tendrá un sabor más intensivo.",
+        }, {
+        "number": 5,
+        "sumary":"Añadir arroz y marisco",
+        "description": "Se puede echar arroz redondo normal o arroz bomba. Éste último absorbe más caldo y se dice que tiene un sabor mejor. Hay que añadir el arroz de forma que se reparta de forma uniforme en toda la paella y que quede cubierto completamente de agua. A veces puede ser necesario poner un poco de agua adicional. Acto seguido se añaden los mejillones y las gambas a la paella.",
+        }, {
+        "number": 6,
+        "sumary":"Dar el color típico",
+        "description": "La paella siempre tiene que tener un color amarillento o dorado. Para ello se puede usar colorante alimentario o azafrán. Para que la paella tome un color uniforme hay que repartir bien el colorante. Ahora se puede probar el caldo para eventualmente añadir algún ingrediente más. Durante la fase de cocción no se puede remover ya el arroz. Esto es una regla muy importante a la hora de cocinar cualquier paella.",
+        }, {
+        "number": 7,
+        "sumary":"El toque final",
+        "description": "La paella mixta estará lista para comer una vez ya no quede caldo en la aella y el arroz esté bien echo. Se recomienda dejar reposar la paella mixta durante unos miutos antes de comenzar a comer. A los valencianos les gusta dejar la paella al fuego un poco más de tiempo con el fin de tostar ligeramente la última capa de arroz. En éste caso hay que tener cuidado de no quemar el arroz."
+        }        
     ],
+    "image": "img/fideua.jpg",
+    "icon": "img/arroz_icon.jpg",
     "important": "true",
     "favourite": [
         {
@@ -104,23 +280,109 @@ var recipes = [
         }, {
         "favList": "Fines de semana",
         }
-    ],
+    ]
+},
+{
+    "id":3,
+    "name": "Solomillo al horno",
+    "subname": "Paella mixta de verduras, pollo y marisco.",
+    "category": "Carnes",
+    "people": "4",
+    "ingredients": [
+        {
+        "ingredient": "Arroz",
+        "quantity": "500",
+        "units" : "gr",
+        "details": ""
+        }, {
+        "ingredient": "Pollo",
+        "quantity": "300",
+        "units" : "gr",
+        "details": "Contramuslo deshuesado"
+		}, {
+	    "ingredient": "Gambas",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Peladas"
+		}, {
+	    "ingredient": "Mejillones",
+		"quantity": "250",
+        "units" : "gr",
+		"details": "Limpios"
+		}, {
+	    "ingredient": "Almejas/Chirlas",
+		"quantity": "200",
+        "units" : "gr",
+		"details": "Limpiarlas antes"
+		}, {
+	    "ingredient": "Sepia",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Cortada en taquitos"
+		}, {
+	    "ingredient": "Tomate frito",
+		"quantity": "1",
+        "units" : "lata de 200gr",
+		"details": ""
+		}, {
+	    "ingredient": "Cebolla",
+		"quantity": "1",
+        "units" : "unidad",
+		"details": "Muy picada"
+		}, {
+	    "ingredient": "Pimiento verde",
+		"quantity": "2",
+        "units" : "unidades",
+		"details": "Muy picado"
+		}, {
+	    "ingredient": "Ajo",
+		"quantity": "1",
+        "units" : "diente",
+		"details": "Laminado"
+		}
+    ],        
     "step": [
         {
         "number": 1,
-        "sumary":"sumary del paso 1",
-        "description": "hay que freir el pollo",
-        "image": "img1"
+        "sumary":"Preparar los ingredientes",
+        "description": "En primer lugar se tienen que limpiar y después cortar todos los ingredientes. Los pimientos rojos se cortan en tiras largas, la cebolla en cubos y el ajo en rodajas finas. La carne se debe cortar en trozos  iguales para que se haga bien. La sepia se corta en trozos más pequeños.",
         }, {
         "number": 2,
-        "sumary":"sumary del paso 2",
-        "description": "se añade el arroz",
-        "image": "img2"
+        "sumary":"Freir la carne y la sepia",
+        "description": "A continuación se fríe la carne. Antes de que esté bien echa se añade la sepia y poco después la verdura. Se fríe todo hasta que la carne tenga un color dorado. Hay que controlar bien el fuego para que no se haga demasiado la carne y que la sepia quede blanda. Se pueden sacar algunos ingredientes de la paella y dejarlos en un plato hasta que el resto esté listo para seguir.",
+        }, {
+        "number": 3,
+        "sumary":"Freir el resto de ingredientes",
+        "description": "A continuación se añade el resto de verdura y el ajo. De nuevo hay que controlar bien el fuego para no quemar ningún ingrediente de la paella mixta. Ahora también se añade la fritada (o sofrito) y el pimentón en polvo. Se deja freír todo durante unos minutos. Este es uno de los últimos pasos antes de añadir el agua o caldo y el arroz.",
+        }, {
+        "number": 4,
+        "sumary":"Añadir caldo",
+        "description": "Para ésta receta se añadirían dos litros de agua. Si se usa arroz bomba se echaría un poco más de agua. De momento no hay nada más que hacer que esperar a que el agua comienze a hervir fuertemente para que la verdura cruda se pueda hacer bien. Sustituyendo el agua por caldo de pollo la paella mixta tendrá un sabor más intensivo.",
+        }, {
+        "number": 5,
+        "sumary":"Añadir arroz y marisco",
+        "description": "Se puede echar arroz redondo normal o arroz bomba. Éste último absorbe más caldo y se dice que tiene un sabor mejor. Hay que añadir el arroz de forma que se reparta de forma uniforme en toda la paella y que quede cubierto completamente de agua. A veces puede ser necesario poner un poco de agua adicional. Acto seguido se añaden los mejillones y las gambas a la paella.",
+        }, {
+        "number": 6,
+        "sumary":"Dar el color típico",
+        "description": "La paella siempre tiene que tener un color amarillento o dorado. Para ello se puede usar colorante alimentario o azafrán. Para que la paella tome un color uniforme hay que repartir bien el colorante. Ahora se puede probar el caldo para eventualmente añadir algún ingrediente más. Durante la fase de cocción no se puede remover ya el arroz. Esto es una regla muy importante a la hora de cocinar cualquier paella.",
+        }, {
+        "number": 7,
+        "sumary":"El toque final",
+        "description": "La paella mixta estará lista para comer una vez ya no quede caldo en la aella y el arroz esté bien echo. Se recomienda dejar reposar la paella mixta durante unos miutos antes de comenzar a comer. A los valencianos les gusta dejar la paella al fuego un poco más de tiempo con el fin de tostar ligeramente la última capa de arroz. En éste caso hay que tener cuidado de no quemar el arroz."
+        }        
+    ],
+    "image": "img/fideua.jpg",
+    "icon": "img/arroz_icon.jpg",
+    "important": "true",
+    "favourite": [
+        {
+        "favList": "Diario",
+        }, {
+        "favList": "Fines de semana",
         }
-    ],    
-        "image": "img/fideua.jpg",
-        "icon": "img/arroz_icon.jpg"
-    }
+    ]
+}
 ];
 
 exports.findAll = function (req, res, next) {
